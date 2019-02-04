dc9c0efb18c7cd1c50fcd5d9c807456d73890d46
javi <gomezjavi2b@gmail.com>
2019-02-04 12:55:27 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+	document.getElementbyId("container").style.display = "flex";
+}
+
+setTimeout(function() {
+	document.getElementbyId("container").style.display="none";
+	},3000);
+}
